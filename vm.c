251014f4163a620fb9dd2a0c0300a6b6ea3206fe
mani-9845 <manipinninti.000@gmail.com>
2026-01-15 17:51:05 +0530

    Implement mark phase for GC
    Added garbage collection mark phase to manage object memory.

--- vm.c
+++ vm.c
@@ -2,6 +2,8 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+#define MARK_STACK_MAX 1000000
+
 static Obj *alloc_object(VM *vm, ObjType type) {
     Obj *obj = malloc(sizeof(Obj));
     if (!obj) exit(1);
@@ -39,4 +41,41 @@ void push(VM *vm, Value v) {
 
 Value pop(VM *vm) {
     return vm->stack[--vm->sp];
+}
+
+/* ===== MARK PHASE ===== */
+
+static void mark_object(Obj *root) {
+    if (!root) return;
+
+    Obj *stack[MARK_STACK_MAX];
+    int top = 0;
+    stack[top++] = root;
+
+    while (top > 0) {
+        Obj *obj = stack[--top];
+        if (obj->marked) continue;
+
+        obj->marked = 1;
+
+        switch (obj->type) {
+            case OBJ_PAIR:
+                if (obj->as.pair.left)  stack[top++] = obj->as.pair.left;
+                if (obj->as.pair.right) stack[top++] = obj->as.pair.right;
+                break;
+            case OBJ_CLOSURE:
+                stack[top++] = obj->as.closure.function;
+                stack[top++] = obj->as.closure.env;
+                break;
+            default:
+                break;
+        }
+    }
+}
+
+static void mark_roots(VM *vm) {
+    for (int i = 0; i < vm->sp; i++) {
+        if (vm->stack[i].type == VAL_OBJ)
+            mark_object(vm->stack[i].as.obj);
+    }
 }
